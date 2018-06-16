e914039a321cc06903724bb443e3a585ab08e2f8
Joseph Timmer <profsmallpine@gmail.com>
2018-06-15 21:31:27 -0600

    Remove videos section + data

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'faq' => 'pages#faq'
-  resources :videos, only: [:index, :show]
   match '/404', :to => 'errors#not_found', :via => :all
   match '/500', :to => 'errors#internal_server_error', :via => :all
 end
